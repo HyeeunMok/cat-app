a7003b39bd67b9efad4104e63cc95bd16c83db19
Hyeeun Mok <hyeeuni.mok@gmail.com>
2022-01-28 15:18:10 -0500

    fix typo

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import cat3 from '../public/images/cat3.gif';
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center w-full h-screen space-y-8 lg-flex-row">
-      <h1 className="text-3xl">Teh Cat App</h1>
+      <h1 className="text-3xl">The Cat App</h1>
       <div className="relative w-32 h-32 overflow-hidden rounded-full ring-2 ring-pink-300 ring-offset-4">
         <Image
           objectFit="cover"
